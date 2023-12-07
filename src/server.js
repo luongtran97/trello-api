0eca1f72d3adbc92426b037ddc92ca0daa67dafc
luong tran <luongluong99783@gmail.com>
2023-12-07 15:02:45 +0700

    Init code base

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 8080
+
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
+})
+
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
+})
