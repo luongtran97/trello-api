4f153b89c238c58ab2a401db9b8921c6b594940a
luong tran <luongluong99783@gmail.com>
2023-12-13 16:42:54 +0700

    express_routing

--- src/server.js
+++ src/server.js
@@ -1,18 +1,15 @@
 /* eslint-disable no-console */
 import express from 'express'
 import exitHook from 'async-exit-hook'
-import { CONNECT_BD, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import { CONNECT_BD, CLOSE_DB } from '~/config/mongodb'
 import 'dotenv/config'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
 
   const app = express()
-
-
-  app.get('/', async (req, res) => {
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Hello Luong Tran Dev, I am running at ${ env.APP_HOST }:${ env.APP_PORT }/`)
