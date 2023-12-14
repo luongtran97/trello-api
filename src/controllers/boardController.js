aabc811b4da2e8af7c22fd50174e107714a0a8c0
luong tran <luongluong99783@gmail.com>
2023-12-14 13:03:07 +0700

    error_handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,11 +1,9 @@
 import { StatusCodes } from 'http-status-codes'
-
 const createNew = async(req, res, next) => {
   try {
-    console.log(req.body)
     res.status(StatusCodes.CREATED).json({ message:'Note: Post from Controller' })
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ errors: error.message })
+    next(error)
   }
 }
 
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/apiError'
 
 const createNew = async(req, res, next ) => {
   const correctCondition = Joi.object({
@@ -12,7 +13,9 @@ const createNew = async(req, res, next ) => {
     //Validate dữ liệu xong, hợp lệ thì cho request đi tiếp sang Controller
     next()
   } catch (error) {
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ errors: new Error(error).message })
+    const errorMessage = new Error(error).message
+    const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage)
+    next(customError)
   }
 }
 export const boardValidation = {
