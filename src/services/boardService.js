d42eafefd6df75dfa73b21e19151a94827c81949
luong tran <luongluong99783@gmail.com>
2023-12-15 16:52:32 +0700

    api_create_board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,4 +1,5 @@
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 const createNew = async(reqBody) => {
   // eslint-disable-next-line no-useless-catch
   try {
@@ -8,12 +9,15 @@ const createNew = async(reqBody) => {
       slug: slugify(reqBody.title)
     }
     // gọi tới tầng model để xử lý lưu bản ghi newBoard vào trong Database
-    //..
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    // lấy bản ghi board sau khi gọi
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
     // làm thêm các xử lý logic khác với các Collection khác tùy đặc thù dự án...
     // bắn email, notification về cho admin khi có 1 cái board mới được tạo..
 
     // trả kết quả về trong service bằng return
-    return newBoard
+    return getNewBoard
   } catch (error) {
     throw error
   }
