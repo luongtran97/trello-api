487a35709741d58bddec3cb8c00b971cab6264cc
luong tran <luongluong99783@gmail.com>
2023-12-27 15:45:58 +0700

    delete_column_and_card

--- src/models/cardModel.js
+++ src/models/cardModel.js
@@ -83,11 +83,20 @@ const update = async(cardId, updateData) => {
     throw new Error(error)
   }
 }
+const deteleManyCardsByColumnId = async(columnId) => {
+  try {
+    const result = await GET_DB().collection(CARD_COLLECTION_NAME).deleteMany({ columnId: new ObjectId(columnId) })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
 export const cardModel = {
   CARD_COLLECTION_NAME,
   CARD_COLLECTION_SCHEMA,
   createNew,
   findOneById,
   pushCardOrderIds,
-  update
+  update,
+  deteleManyCardsByColumnId
 }
--- src/models/columnModel.js
+++ src/models/columnModel.js
@@ -78,11 +78,20 @@ const update = async(columnId, updateData) => {
     throw new Error(error)
   }
 }
+const deleteColumn = async(columnId) => {
+  try {
+    const result = await GET_DB().collection(COLUMN_COLLECTION_NAME).deleteOne({ _id: new ObjectId(columnId) })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
 export const columnModel = {
   COLUMN_COLLECTION_NAME,
   COLUMN_COLLECTION_SCHEMA,
   createNew,
   findOneById,
   pushCardOrderIds,
-  update
+  update,
+  deleteColumn
 }
